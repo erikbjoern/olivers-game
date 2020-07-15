20c92735ab28e2b5a4c99d8a75255108dcf639d6
Erik Björn <erik.g.bjoern@gmail.com>
2020-07-15 19:54:09 +0200

    adding ability to move left

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from "react";
-import logo from "./figur.png";
+import figur from "./figur.png";
 import "./App.css";
 
 class App extends Component {
   state = {
     left: 0,
-    keyIsPressed: false,
   };
 
   componentDidMount() {
-    document.addEventListener("keydown", this.keyDownHandler);
-    document.addEventListener("keyup", this.keyUpHandler);
+    document.addEventListener("keydown", (event) => this.keyDownHandler(event));
   }
 
-  keyDownHandler = () => {
-    this.setState({ left: this.state.left + 10 })
-  };
-
-  keyUpHandler = () => {
-    this.setState({ keyIsPressed: false })
+  keyDownHandler = (event) => {
+    if (event.keyCode === 39 && this.state.left < 990) {
+      this.setState({ left: this.state.left + 10 });
+    }
+    if (event.keyCode === 37 && this.state.left >= 10) {
+      this.setState({ left: this.state.left - 10})
+    }
   };
 
   render() {
@@ -26,9 +25,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img
-            src={logo}
+            src={figur}
             className="App-logo"
-            alt="logo"
+            alt="figur"
             style={{ position: "absolute", left: this.state.left }}
           />
         </header>
